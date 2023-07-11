4c555156588135155b746f7d1b2cf4386334e616
flowerkong3 <90238684+flowerkong3@users.noreply.github.com>
2023-07-11 17:58:12 +0900

    Delete js/components directory

--- js/components/hamburger.js
+++
@@ -1,7 +0,0 @@
-// Mobile Toggle Menu 
-const hamburger = document.querySelector(".hamburger")
-const gnb = document.querySelector(".gnb")
-function toggleHamburger(){
-	hamburger.classList.toggle("active");
-	gnb.classList.toggle("active");
-}
